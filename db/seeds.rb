e6b88be6ef17fd908593cc27e36809cbc92c9048
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-09-21 11:36:49 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeds for vehicles
+
+Vehicle.destroy_all
+Passenger.destroy_all
+Travel.destroy_all
+
+# Seeds for vehicles
+vehicles = [
+  { color: 'white', model: '2015', licence_plate: 'ABC234' },
+  { color: 'red', model: '2013', licence_plate: 'ABC564' },
+  { color: 'black', model: '2016', licence_plate: 'DFG564' },
+  { color: 'white', model: '2011', licence_plate: 'DFG872' }
+]
+
+persisted_vehicles = vehicles.map do |vehicle|
+  vehicle = Vehicle.new(vehicle)
+  Vehicles::LicencePlateParser.call(vehicle)
+  vehicle.save!
+  vehicle
+end
+
+puts "Vehicles created"
+
+# Seeds for passengers
+passengers = [
+  { name: 'Andres' }, { name: 'Cristian' }, { name: 'Camilo' }, { name: 'Juan' }
+]
+
+persisted_passengers = passengers.map do |passenger|
+  passenger = Passenger.new(passenger)
+  passenger.save!
+  passenger
+end
+puts "Passengers created"
+
+# Seeds for travels
+travels = [
+  { origin: 'Medellín', destination: 'Envigado' },
+  { origin: 'Medellín', destination: 'Sabaneta' },
+  { origin: 'Sabaneta', destination: 'Robledo' },
+  { origin: 'Envigado', destination: 'La estrella' }
+]
+
+travels.each_with_index do |hash, index|
+  travel = Travel.new(hash).tap do |tr|
+    tr.vehicle    = persisted_vehicles[index]
+    tr.passenger  = persisted_passengers[index]
+  end
+  travel.save!
+end
+
+puts "Travels created"
