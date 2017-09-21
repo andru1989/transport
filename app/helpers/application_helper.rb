494d79d26de90b2e7c873b44961761827de20ea9
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-09-21 11:17:47 -0500

    create helper to translate enums and labels in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def t_label(key)
+    I18n.t "views.#{key}"
+  end
+
+  def translate_enum(object, attribute)
+    if object.public_send(attribute).present?
+      I18n.t("activerecord.attributes.#{object.class.to_s.underscore}.#{attribute.to_s.pluralize}.#{object.public_send(attribute)}")
+    end
+  end
 end
